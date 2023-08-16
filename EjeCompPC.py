d83667270c9f9c11ad825daefefd8b8a6b4ec742
ThomasMunoz27 <141789856+ThomasMunoz27@users.noreply.github.com>
2023-08-16 14:57:51 -0300

    Add files via upload

---
+++ EjeCompPC.py
@@ -0,0 +1,74 @@
+#1
+numero1 = 4
+print(numero1)
+
+#2
+numero2 = 4.4444
+print(numero2)
+
+#3
+suma = numero1 + numero2
+print(suma)
+
+#4
+resta = numero1 - numero2
+print(resta)
+
+multiplicacion = numero1 * numero2
+print(multiplicacion)
+
+division = numero1 / numero2
+print(division)
+
+#5
+nombre = "Thomas"
+print(nombre)
+
+#6
+precio = 1500
+print(precio)
+
+#7
+descuento = 0.15
+print(descuento)
+
+#8
+precio_final = precio-(precio*descuento)
+print(precio_final)
+
+#9
+cadena = "Mañana voy a comer pastel de papa"
+print(cadena)
+
+#10
+longitud = len(cadena)
+print(longitud)
+
+#11
+precio = int(1499.99)
+print(precio)
+
+#12
+nombre = "Thomas"
+apellido = "Muñoz"
+nombre_completo = nombre + apellido
+print(f"{nombre} {apellido}")
+
+#13
+edad = 18
+edad +=5
+edad -=10
+print(edad)
+
+#14
+altura = 1.70
+altura *=4
+altura /=3
+print(altura) 
+
+#15
+nombre_m = "THOMAS"
+print(nombre_m.lower())
+
+#16
+print(nombre_m.title())
