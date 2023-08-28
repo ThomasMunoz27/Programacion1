6b625a4003488285e46c3e912ff7f8f853133ee2
ThomasMunoz27 <thomasnm2004@gmail.com>
2023-08-28 14:07:00 -0300

    aaa

--- Tp 2 Codigo.py
+++ Tp 2 Codigo.py
@@ -139,7 +139,14 @@ elif tipo_pizza == "normal":
         print("Ese ingrediente no está disponible")
 
 
-#Ejercicio 12
+ano_actual, ano_cualquiera = int(input("Ingrese el año actual: ")), int(input("Ingrese un año cualquiera: "))
+
+if ano_actual < ano_cualquiera:
+    ano_diff = ano_cualquiera - ano_actual
+    print(f"Faltan {ano_diff} años para llegar a {ano_cualquiera}")
+else:
+    ano_diff = ano_actual - ano_cualquiera
+    print(f"Han pasado {ano_diff} años desde {ano_cualquiera}")
 
 
 #Ejercicio 19
