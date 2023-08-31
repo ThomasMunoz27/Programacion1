8a0308757095afaa19d4c79123719c2f83d7fe94
ThomasMunoz27 <thomasnm2004@gmail.com>
2023-08-30 21:19:39 -0300

    Ejercicios bucles

--- EjerciciosBucles.py
+++ EjerciciosBucles.py
@@ -12,3 +12,25 @@ for cant_msg in range(5):
         else:
             msg_encriptado = msg_encriptado + abc[(indice + corr)%27]
     print(msg_encriptado)
+
+
+
+
+#Ejercicio while
+
+parar = 1
+pares = 0
+impares = 0
+while parar !=0:
+    num = int(input("Ingrese números enteros positivos. Para detenerse presione 0"))
+    if num == 0:
+        parar = 0
+    elif num < 0:
+        print("Número ingresado inválido")
+    else:
+        if num % 2 == 0:
+            pares = pares +1
+        else:
+            impares = impares + 1
+
+print(f"Se han ingresado {pares} números pares y {impares} números impares")
