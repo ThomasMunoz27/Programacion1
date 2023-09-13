9fb93419a566536fb7918167230422d1ac305b26
ThomasMunoz27 <thomasnm2004@gmail.com>
2023-09-12 22:59:15 -0300

    agregar enunciado

--- aRepaso parcial/UniendoCodigo.py
+++ aRepaso parcial/UniendoCodigo.py
@@ -1,6 +1,17 @@
+#Tienda de ropa
+"""Geier Indumentaria.
+Es un programa que prepara una orden de compra. Primeramente nos presenta un menú con las opciones “Comprar”, “Consultar stock” y “Forma de pago y descuentos”. Seleccionando “2”  muestra la lista de artículos con sus respectivos precios. Para darle al cliente la opción de comprar si lo desea o “salir”. Seleccionando “3” nos da información de las alternativas de pago. El precio del flete, recargos si abona con tarjeta o descuento si abona en efectivo. Y seleccionando “1” nos lleva a la compra. Nos muestra la lista de artículos con los precios. Luego tenemos que indicar la cantidad de tipo de artículos que vamos a comprar y de acuerdo a esto el programa solicita el código del artículo y la cantidad que queremos del mismo. Una vez especificado todos los artículos  nos pregunta sobre el modo de pago “tarjeta/efectivo”. Al indicar un método el programa calcula la recarga o el descuento correspondiente y si suma el flete.
+Por último muestra el detalle de la compra: cantidad, artículo, precio total por artículo,
+y monto total de la compra.
+"""
+
+
 # Variables del stock
 list_article = ["Pantalones", "Camisas", "Remeras", "Faldas", "Corbatas", "Jogging"]
 list_price = [5000, 7500, 3990, 6000, 2400, 6750]
+
+
+
 
 # Variables del precio
 payment_validation = 0
@@ -14,7 +25,10 @@ print("menú 1: Compras ")
 print("menú 2: Consultar stock. ")
 print("menú 3: Forma de pago y descuentos: ")
 
-    # Ciclo para ingresar al menú y también de validación del primer dato de entrada
+
+
+
+    # Ciclo para ingresar al menú y también de validación del primer dato de cod_article
 while option_menu != 0:
     option_menu =(input("Por favor, ingrese [1] para el menú 1, [2] para el menú 2, [3] para el menú 3 o [0] para salir: "))
     # Verificar si la opción ingresada es válida
@@ -31,35 +45,58 @@ while option_menu != 0:
             list_article = ["Pantalones", "Camisas", "Remeras", "Faldas", "Corbatas", "Jogging"]
             list_price = [5000, 7500, 3990, 6000, 2400, 6750]
 
+
+
+
             for i in range(6):
                 print(f"(cod:{i + 1}). {list_article[i]} ${list_price[i]}")
 
+
+
+
             order = {}
             article_types = int(input("¿Cuántos tipos de artículos contiene el pedido?: "))
+
+
+
 
             if article_types > 6:
                 print("Número ingresado incorrecto")
             else:
                 # Pedir información sobre los artículos a comprar
                 for n in range(article_types):
-                    cod_article = int(input("Ingrese el código del artículo: "))
-
+                    while True:
+                        cod_article = (input("Ingrese el código del artículo: "))
+                        if len(cod_article) == 1 and cod_article.isdigit():
+                            break
+                        else:
+                            print("Error. Debes ingresar solo un dígito.")
+                    cod_article = int(cod_article)
                     if (cod_article < 1) or (cod_article > 6):
                         print("Número ingresado incorrecto")
                     else:
                         article = list_article[cod_article - 1]
                         amount = int(input(f"Ingresar la cantidad de {article} que desea pedir: "))
 
+
+
+
                         if article in order:
                             order[article] += amount
                         else:
                             order[article] = amount
+
+
+
 
                         price = list_price[cod_article - 1] * amount
                         sub_total += price
                 #Preguntar método de pago hasta que se ingrese un valor válido
                 while payment_validation == 0:
                     payment_method = input(f"¿Cuál va a ser el método de pago? \nEfectivo/Tarjeta\n").lower()
+
+
+
 
                     if payment_method == "efectivo":
                         if sub_total > 10000:
@@ -78,11 +115,17 @@ while option_menu != 0:
             for article, amount in order.items():
                 print(f'Se compraron {amount} de {article} ${list_price[list_article.index(article)] * amount}')
 
+
+
+
             print(f"El precio final de la compra es: ${total}")
         # Opción para consultar stock
         elif option_menu == 2:
             print("Usted ha ingresado 2. Consultar stock: ... ")
             print("Stock de artículos: ")
+
+
+
 
             for i in range(6):
                 print(f"(cod:{i + 1}). {list_article[i]} - Precio unitario: ${list_price[i]} ")
@@ -98,4 +141,7 @@ while option_menu != 0:
     # Si el valor ingresado no es válido
     else:
         print("Valor ingresado inválido")
+
+
+
 
