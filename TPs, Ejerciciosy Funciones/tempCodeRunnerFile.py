0c8bb1a3685d184df1d2be04cc2931b8dbbe8bc3
ThomasMunoz27 <thomasnm2004@gmail.com>
2023-10-19 13:35:07 -0300

    Empezando tp 8

--- TPs, Ejerciciosy Funciones/tempCodeRunnerFile.py
+++ TPs, Ejerciciosy Funciones/tempCodeRunnerFile.py
@@ -1,19 +1,6 @@
-#Ejercicio 1 y 2
-num_list = []
-while True:
-    try:
-        entry_num = int(input("Ingrese números para ingresarlos en una lista: "))
-        if entry_num == 0:
-            break
-        else:
-            num_list.append(entry_num)
+import aFunciones as funcion
+n = int(input("Ingrese la fila:  "))
+k = int(input("Ingrese la columna: "))
 
-
-    except ValueError:
-        print("Ingrese un número")
-second_entry = int(input("Ingrese otro número"))
-if second_entry in num_list:
-    num_list.remove(second_entry)
-    print(num_list)
-else: 
-    print("No es posible eliminar ese número de la lista")
+num = funcion.pascal(n,k)
+print(num)
